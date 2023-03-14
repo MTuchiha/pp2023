4c2ad02b022cf4147cbe82c0fb834f5ff10af4f8
MTuchiha <tungnm.bi12-483@st.usth.edu.vn>
2023-03-14 20:13:40 +0700

    add menu labwork2

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -17,6 +17,8 @@ class student:
         self.__id = id
     def set_dob(self, dob):
         self.__dob = dob
+    def __str__(self):
+        return f"Name: {self.__name}, ID: {self.__id}, DOB: {self.__dob}"
     
 class course:
     marks = []
@@ -40,6 +42,8 @@ class course:
         for i in self.marks:
             total += i
         return total/len(self.marks)
+    def __str__(self):
+        return f"Name: {self.__name}, ID: {self.__id}"
 
 def input_number_students():
     return int(input("Enter number of students: "))
@@ -70,27 +74,56 @@ def input_mark(courses, students):
 def list_courses(courses):
     print("List of courses:")
     for course in courses:
-        print(f"-{course.get_name()}")
+        print(course)
 
 def list_students(students):
     print("List of students:")
     for student in students:
-        print(f"-{student.get_name()}")
+        print(student)
 
 def show_course_marks(course):
     for mark in course.get_marks():
-        print(f"-{mark}")
+        print(f"{mark}")
+    print(f"Average: {course.get_average()}")
 
+def menu():
+    print("1. Input students info")
+    print("2. Input courses info")
+    print("3. Input marks")
+    print("4. List courses")
+    print("5. List students")
+    print("6. Show course marks")
+    print("7. Show student info")
+    print("8. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice ==1:
+        input_students_info()
+    elif choice ==2:
+        input_courses_info()
+    elif choice ==3:
+        input_mark(courses, students)
+    elif choice ==4:
+        list_courses(courses)
+    elif choice ==5:
+        list_students(students)
+    elif choice ==6:
+        choose = input("Enter your course_id: ")
+        for course in courses:
+            if course.get_id() == choose:
+                show_course_marks(course)
+    elif choice ==7:
+        choose2 = input("Enter your student_id: ")
+        for student in students:
+            if student.get_id() == choose2:
+                print(student)
+    elif choice ==8:
+        exit()
+while True:
+    menu()
 
-input_students_info()
-input_courses_info()
-input_mark(courses, students)
-list_courses(courses)
-list_students(students)
-for course in courses:
-    print(f"List of marks for {course.get_name()}:")
-    show_course_marks(course)
-    print(f"Average mark for {course.get_name()}: {course.get_average()}")
+    
+        
+        
 
 
 
